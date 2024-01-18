616922f5011724d1ea8c5b4077da72837e0da543
Poonam Sangle <poonamsangle19@gmail.com>
2024-01-17 22:02:05 -0800

    Initial commit for Playwright Test

--- tests/tech-example.test.ts
+++ tests/tech-example.test.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test'; //Import the selectors into a t
 
 import {
   getDemoButtonByCssSelector,
-  getByRoleSelector,
+  getIframeSelector,
 } from '../pages/tech-example.page';
 
 test('Navigate to https://arkestro.com/ and check something on the page', async ({page, }) => {
@@ -10,6 +10,6 @@ test('Navigate to https://arkestro.com/ and check something on the page', async 
         await expect(page.getByText('Arkestro drives better buying decisions, faster, at scale!')).toBeVisible();
         await getDemoButtonByCssSelector(page).click();
         await expect(page.getByText('Request a custom demo')).toBeVisible();
-        await getIframeSelector(page).getByLabel('Email*').fill('test@gmail.com');
+        await getIframeSelector(page).getByLabel('Email').fill('test@gmail.com');
         await expect(getIframeSelector(page).getByText('Please enter a different email address. This form does not accept addresses from gmail.com.')).toBeVisible();
 });
